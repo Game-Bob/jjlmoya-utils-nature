4801775b474ed0bc62b6cba9f45edc1d6e84fb78
jjlmoya <juanjo1989@gmail.com>
2026-04-12 14:59:47 +0200

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -18,13 +18,13 @@ export type {
 export { ALL_TOOLS } from './tools';
 
 export { CricketThermometerComponent, CricketThermometerSEO, CricketThermometerBibliography } from './tool/cricketThermometer';
-export { CRICKET_THERMOMETER_TOOL } from './tool/cricketThermometer/index';
+export { CRICKET_THERMOMETER_TOOL, cricketThermometer } from './tool/cricketThermometer/index';
 
 export { SeedCalculatorComponent, SeedCalculatorSEO, SeedCalculatorBibliography } from './tool/seedCalculator';
-export { SEED_CALCULATOR_TOOL } from './tool/seedCalculator/index';
+export { SEED_CALCULATOR_TOOL, seedCalculator } from './tool/seedCalculator/index';
 
 export { RainHarvesterComponent, RainHarvesterSEO, RainHarvesterBibliography } from './tool/rainHarvester';
-export { RAIN_HARVESTER_TOOL } from './tool/rainHarvester/index';
+export { RAIN_HARVESTER_TOOL, rainHarvester } from './tool/rainHarvester/index';
 
 export { DigitalCarbonComponent, DigitalCarbonSEO, DigitalCarbonBibliography } from './tool/digitalCarbon';
-export { DIGITAL_CARBON_TOOL } from './tool/digitalCarbon/index';
+export { DIGITAL_CARBON_TOOL, digitalCarbon } from './tool/digitalCarbon/index';

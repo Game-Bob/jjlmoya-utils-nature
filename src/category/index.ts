d63aa538b1cf9895bf00db9ee5b86f99c8447f5b
jjlmoya <juanjo1989@gmail.com>
2026-04-12 14:45:47 +0200

    fix: exports

--- src/category/index.ts
+++ src/category/index.ts
@@ -1,10 +1,12 @@
 import type { NatureCategoryEntry } from '../types';
 import { cricketThermometer } from '../tool/cricketThermometer/index';
 import { seedCalculator } from '../tool/seedCalculator/index';
+import { rainHarvester } from '../tool/rainHarvester/index';
+import { digitalCarbon } from '../tool/digitalCarbon/index';
 
 export const natureCategory: NatureCategoryEntry = {
   icon: 'mdi:leaf',
-  tools: [cricketThermometer, seedCalculator],
+  tools: [cricketThermometer, seedCalculator, rainHarvester, digitalCarbon],
   i18n: {
     es: () => import('./i18n/es').then((m) => m.content),
     en: () => import('./i18n/en').then((m) => m.content),

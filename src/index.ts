3cee513ee97e139327309a0587b21b1c5f48dce1
jjlmoya <juanjo1989@gmail.com>
2026-04-18 21:28:54 +0200

    fix: remove re-exports of removed component vars from main index

--- src/index.ts
+++ src/index.ts
@@ -17,14 +17,10 @@ export type {
 
 export { ALL_ENTRIES, ALL_TOOLS } from './tools';
 
-export { CricketThermometerComponent, CricketThermometerSEO, CricketThermometerBibliography } from './tool/cricketThermometer';
 export { CRICKET_THERMOMETER_TOOL, cricketThermometer } from './tool/cricketThermometer/index';
 
-export { SeedCalculatorComponent, SeedCalculatorSEO, SeedCalculatorBibliography } from './tool/seedCalculator';
 export { SEED_CALCULATOR_TOOL, seedCalculator } from './tool/seedCalculator/index';
 
-export { RainHarvesterComponent, RainHarvesterSEO, RainHarvesterBibliography } from './tool/rainHarvester';
 export { RAIN_HARVESTER_TOOL, rainHarvester } from './tool/rainHarvester/index';
 
-export { DigitalCarbonComponent, DigitalCarbonSEO, DigitalCarbonBibliography } from './tool/digitalCarbon';
 export { DIGITAL_CARBON_TOOL, digitalCarbon } from './tool/digitalCarbon/index';
